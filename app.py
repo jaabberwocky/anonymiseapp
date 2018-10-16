61fd8bef72c2c3200b32fae0c028ad241e6b3ffc
tobias <tobiasleongzhunmun@gmail.com>
2018-10-17 00:11:04 +0800

    added more tests
    fixed the inability to post files

--- app.py
+++ app.py
@@ -12,16 +12,13 @@ import uuid
 application = Flask(__name__)
 bootstrap = Bootstrap(application)
 
-try:
-	# load configurations json (kept secret)
-	configurations = json.loads(open("config.json", "r").read())
+# load configurations json (kept secret)
+configurations = json.loads(open("config.json", "r").read())
 
-	# set file destination
-	application.config['UPLOADED_DATAFILES_DEST'] = configurations["SAVE_FILE_DESTINATION"]
-	# set secret
-	application.secret_key = configurations['SECRET_KEY']
-except FileNotFoundError:
-	pass
+# set file destination
+application.config['UPLOADED_DATAFILES_DEST'] = configurations["SAVE_FILE_DESTINATION"]
+# set secret
+application.secret_key = configurations['SECRET_KEY']
 
 # declare files for upload set
 # DATA allows for only data extensions (".csv" etc)
@@ -148,4 +145,4 @@ def selectcolumn():
 
 
 if __name__ == "__main__":
-	application.run(debug=True)
+	application.run(debug=True, port=80)
--- tests.py
+++ tests.py
@@ -1,15 +1,19 @@
 import unittest
+from flask_testing import TestCase
 import shutil
 import hashlib
 import os
 import sys
+from bs4 import BeautifulSoup as bs
+from flask import url_for
 from create_environment import create_environment
 from generatetest import generatetest
 
 # load in application
-from app import hashthis, anonymise, application, returnhtmlview
+from app import hashthis, anonymise, application, returnhtmlview, returncolnames
 
-class ApplicationTests(unittest.TestCase):
+
+class ApplicationTests(TestCase):
 
     def setUp(self):
         ''' make sure that the test_data set is created each time
@@ -17,9 +21,16 @@ class ApplicationTests(unittest.TestCase):
         create_environment()
         generatetest()
         application.config['Testing'] = True
-        
+        self.client = application.test_client()
+
     def tearDown(self):
         shutil.rmtree("data")
+
+    def create_app(self):
+
+        app = application
+        app.config['TESTING'] = True
+        return app
 
     def test_hashthis(self):
         '''
@@ -30,20 +41,76 @@ class ApplicationTests(unittest.TestCase):
         hash_string = salt + s
         hashed = hashlib.sha256(hash_string.encode()).hexdigest()
         self.assertEqual(hashed, hashthis("hello world", "salt"))
-    
+
     def test_anonymise_exists(self):
         '''
         checks if the anonymise function has created a processed file
         '''
         completed_filename = anonymise('test_data.csv', column='id')
-        self.assertTrue(os.path.isfile(os.path.join(os.getcwd(),"data",str(completed_filename) + ".csv")))
+        self.assertTrue(os.path.isfile(os.path.join(
+            os.getcwd(), "data", str(completed_filename) + ".csv")))
 
     def test_htmlview(self):
         '''
-        checks if returnhtmlview returns something
+        checks if returnhtmlview returns valid html
         '''
         html = returnhtmlview('test_data.csv')
-        self.assertIsNotNone(html)
+        self.assertTrue(bool(bs(html, "html.parser").find()))
+
+    def test_returncolnames(self):
+        '''
+        checks if returncolnames returns the correct colnames for test_data (id, names)
+        '''
+        colnames = returncolnames('test_data.csv')
+        self.assertEqual(colnames, ['id', 'names'])
+
+    def test_index(self):
+        '''
+        check if index is 200
+        '''
+
+        resp = client.get('/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_upload_render(self):
+        '''
+        check if upload is 200
+        '''
+        client = application.test_client()
+        resp = client.get('/upload')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_upload_redirect(self):
+        '''
+        check if upload is working correctly with redirect
+        '''
+        client = application.test_client(use_cookies=True)
+        data = dict()
+        # we use this to OPEN our test data created in setUp
+        # make sure follow_redirects is not specified, otherwise you will get 200 response
+        # from '/selectcolumn'
+        with open(os.path.join(os.getcwd(), "data", "test_data.csv"), 'rb') as f:
+            data['datafile'] = (f, f.name)
+            resp = client.post(
+                '/upload',
+                data=data,
+                headers={'content-Type': 'multipart/form-data'})
+        self.assertRedirects(resp, "/selectcolumn")
+
+    def test_selectcolumn(self):
+        '''
+        check if selectcolumn post is working correctly
+        '''
+        with client as client:
+            with client.session_transaction() as sess:
+                sess['filename'] = "test_data.csv"
+            resp = client.post(
+                '/selectcolumn',
+                data=dict(salt="",
+                          column="id"))
+            self.assertRedirects(resp, "/processfile")
+            )
+
 
 if __name__ == "__main__":
     unittest.main()
